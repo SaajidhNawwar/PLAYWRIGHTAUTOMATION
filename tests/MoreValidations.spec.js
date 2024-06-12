76b6d83040435350e859d376f598042c7669f001
SaajidhNawwar <mnmsaajidh@gmail.com>
2024-06-12 14:00:38 +0530

    more validations

---
+++ tests/MoreValidations.spec.js
@@ -0,0 +1,22 @@
+const {test,expect} = require('@playwright/test');
+
+test("Popup Validations", async({page}) =>{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+    //await page.pause();
+
+    //handling dialog popups
+    page.on('dialog', dialog => dialog.accept());
+    await page.locator("#confirmbtn").click();
+
+    //handling hover
+    await page.locator("#mousehover").hover();
+
+    //handle elements in iFrame
+    const framePage = page.frameLocator("#courses-iframe");
+    await framePage.locator("li a[href*='lifetime-access']:visible").click();
+    const textCheck = await framePage.locator(".text h2").textContent();
+    console.log(textCheck.split(" ")[1]);
+})
